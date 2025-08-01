0a3c036f02d8b056c70e6d60db6d1ce421ff195b
Aakash323 <aakashpaudyal9@gmail.com>
2025-08-01 22:16:00 +0545

    errors fixed

--- prisma/migrations/20250801155859_fields/migration.sql
+++ prisma/migrations/20250801155859_fields/migration.sql
@@ -12,9 +12,13 @@ CREATE TABLE "public"."User" (
 -- CreateTable
 CREATE TABLE "public"."Product" (
     "pid" INTEGER NOT NULL,
+    "userId" INTEGER,
 
     CONSTRAINT "Product_pkey" PRIMARY KEY ("pid")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
